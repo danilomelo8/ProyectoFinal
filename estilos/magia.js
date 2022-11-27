d3ae9a69e6817a7566c0ccd126abfc304f514512
danilo melo <danilomelo1984@gmail.com>
2022-11-27 19:45:31 -0300

    proyecto

--- estilos/magia.js
+++
@@ -1,7 +0,0 @@
-var grupoA= {
-    equipo1:"brasil",
-    equipo2:"ecuador",
-    equipo3:"paises-bajos",
-    equipo4:"senegal"
-
-}
--- javascript/magia.js
+++ javascript/magia.js
@@ -1,30 +1,30 @@
-function Enviar(){
+function CalcularResultado(){
     const USUARIOS = 4;
-    const PARTIDOS = 6;
-    let puntaje;
+    const PARTIDOS = 64;
+    let puntaje = 0;
     for (uid=0; uid < USUARIOS; uid++){
         puntaje = 0;
         for(pid = 0; pid < PARTIDOS; pid++){ 
-            usr_loc = parseInt(document.getElementById("usr"+ uid+"_loc"+ pid).value);
-            usr_vis = parseInt(document.getElementById("usr"+ uid+"_vis"+ pid).value);
-            adm_loc = parseInt(document.getElementById("adm_loc"+ pid).value);
-            adm_vis = parseInt(document.getElementById("adm_vis"+ pid).value);
+             let usr_loc = parseInt(document.getElementById("usr"+ uid+"_loc"+ pid).value);
+             let usr_vis = parseInt(document.getElementById("usr"+ uid+"_vis"+ pid).value);
+             let adm_loc = parseInt(document.getElementById("adm_loc"+ pid).value);
+             let adm_vis = parseInt(document.getElementById("adm_vis"+ pid).value);
             if ( isNaN(usr_loc) || isNaN(usr_vis) || isNaN(adm_loc) || isNaN(adm_vis) ) {
                 puntaje += 0; // partido no jugado
             } else if ( (usr_loc == adm_loc) && (usr_vis == adm_vis) ) {
-                puntaje += 5; // envoco resultado exacto
+                puntaje += 3; // envoco resultado exacto
             } else if (
                 ( (usr_loc > usr_vis) && (adm_loc > adm_vis) ) ||
                 ( (usr_loc < usr_vis) && (adm_loc < adm_vis) ) ||
                 ( (usr_loc == usr_vis) && (adm_loc == adm_vis) )
             ) {
-                puntaje += 3; // envoco resultado
+                puntaje += 1; // envoco resultado
             } else {
                 puntaje += 0; // no envoco resultado
             }
         }
         document.getElementById("puntajeusr" + uid).value = puntaje;
+        console.log(puntaje);
     }    
 }
-    
-    
+
